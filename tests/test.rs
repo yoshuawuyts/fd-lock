7dbcfda8f4ecaec4b532af8e387f808bf4ef550d
Sam Wilson <tecywiz121@hotmail.com>
2020-09-11 15:35:05 -0400

    Determine cause of failure from errno

--- tests/test.rs
+++ tests/test.rs
@@ -1,6 +1,21 @@
-use std::error::Error;
+use fd_lock::{ErrorKind, FdLock};
+
+use std::fs::File;
+
+use tempfile::tempdir;
 
 #[test]
-fn should_work() -> Result<(), Box<dyn Error + Send + Sync + 'static>> {
-    Ok(())
+fn double_lock() {
+    let dir = tempdir().unwrap();
+    let path = dir.path().join("lockfile");
+
+    let mut l0 = FdLock::new(File::create(&path).unwrap());
+    let mut l1 = FdLock::new(File::open(path).unwrap());
+
+    let g0 = l0.lock().unwrap();
+
+    let err = l1.try_lock().unwrap_err();
+    assert!(matches!(err.kind(), ErrorKind::Locked));
+
+    drop(g0);
 }
